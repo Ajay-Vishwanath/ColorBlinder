fe5a343a8bc167a0875dc3634c046b0268a2253d
Ajay <ajayvishwanath.94@gmail.com>
2019-11-07 17:16:19 -0800

    Fix React-Navigation-Stack Bug

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack"
+
 import Home from './screens/Home';
 
 const AppNavigator = createStackNavigator({
