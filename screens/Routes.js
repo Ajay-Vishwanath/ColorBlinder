f3c7b6142091a2fddadd1faca28a5387504b411b
Ajay <ajayvishwanath.94@gmail.com>
2019-12-06 16:21:47 -0800

    Start Section 3 of Tutorial

--- screens/Routes.js
+++ screens/Routes.js
@@ -1,12 +1,16 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack"
 import Home from './Home/index'
+import Game from "./Game";
 
 const AppNavigator = createStackNavigator(
     {
         Home: {
             screen: Home
         },
+        Gmame: {
+            screen: Game
+        }
     },
     {
         initialRouteName: "Home",
